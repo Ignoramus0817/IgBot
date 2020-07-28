ca8475db99eaddb03b20c25d0156b8d857dff12c
Ignora <Ignoramus0817@gmail.com>
2020-07-28 16:33:31 +0800

    ip

--- bot.py
+++ bot.py
@@ -3,4 +3,4 @@ import nonebot
 if __name__ == '__main__':
     nonebot.init()
     nonebot.load_builtin_plugins()
-    nonebot.run(host='0.0.0.0', port=8080)
+    nonebot.run(host='172.17.0.1', port=8080)
