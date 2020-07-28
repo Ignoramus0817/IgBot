9ce56b9b29c5b7adca5430c297b7b1e6cb90df26
Ignoramus <Ignoramus0817@gmail.com>
2020-07-28 23:45:26 +0800

    fix few bugs

--- plugins/dice.py
+++ plugins/dice.py
@@ -11,25 +11,26 @@ async def roll(session: CommandSession):
     for i in range(n_dices):
         res.append(random.randint(1, n_faces))
 
-    print(res)
-    await session.send(res)
+    await session.send(" | ".join(res))
 
 
 @roll.args_parser
 async def _(session: CommandSession):
     stripped_arg = session.current_arg_text.strip()
-    splited = [0, 0]
 
     if stripped_arg:
         splited = stripped_arg.lower().split('d')
 
-        if splited[0] != 0 and splited[1] != 0:
-            session.state['n_dices'] = int(splited[0])
-            session.state['n_faces'] = int(splited[1])
-        elif splited[0] == 0 and splited[1] != 0:
-            session.pause('请指定骰子个数')
+        if len(splited) == 2:
+            if splited[0] > 0 and splited[1] > 0:
+                session.state['n_dices'] = int(splited[0])
+                session.state['n_faces'] = int(splited[1])
+            elif splited[0] <= 0 and splited[1] > 0:
+                session.pause('请指定正确的骰子个数')
+            else:
+                session.pause('请指定正确的骰子种类')
         else:
-            session.pause('请指定骰子种类')
+            session.pause('请输入完整的骰点参数')
     return
 
     if not stripped_arg:
