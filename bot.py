375c828464aaa419844f9c7318ab94e24d5019c6
Ignoramus <Ignoramus0817@gmail.com>
2020-07-28 21:49:57 +0800

    add plugins

--- bot.py
+++ bot.py
@@ -1,6 +1,13 @@
 import nonebot
+from os import path
+
+import config
+
 
 if __name__ == '__main__':
-    nonebot.init()
-    nonebot.load_builtin_plugins()
-    nonebot.run(host='172.17.0.1', port=8080)
+    nonebot.init(config)
+    nonebot.load_plugins(
+        path.join(path.dirname(__file__), 'plugins'),
+        'plugins'
+    )
+    nonebot.run()
---
+++ config.py
@@ -0,0 +1,6 @@
+from nonebot.default_config import *
+
+SUPERUSERS = {751029650}
+
+HOST = '172.17.0.1'
+PORT = 8080
