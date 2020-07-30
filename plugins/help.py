97b9f3913ff1d271d0ac7a33faf677676584d95a
Ignoramus <Ignoramus0817@gmail.com>
2020-07-30 21:23:18 +0800

    modify help text

--- plugins/help.py
+++ plugins/help.py
@@ -5,11 +5,11 @@ from nonebot import on_command, CommandSession
 @on_command('help', aliases=['h'])
 async def helper(session: CommandSession):
     comment_text = "【此处为声明】 \n\n " + \
-                   "1. 帮助文本中，()代表参数，[]代表可选。 \n\n" + \
-                   "2. 除单独说明的命令外，其余命令均可用 /, !, ／, ！, .作为命令头。"
+                   "1. 帮助文本中，()代表参数，[]代表可选内容。 \n\n" + \
+                   "2. 除单独说明的命令外，其余命令均可用   /   !   ／   ！   .   作为命令头。"
 
     help_text = "【此处为正文】 \n\n " + \
-                "1. 使用 rd 来进行投点，格式为 .rd (a)d(b) [+(m) [d(n)] ] ，其中b和n在1-100之间。 \n\n " + \
+                "1. 使用 rd 来进行投点，格式为 .rd  (a)d(b)  [+  (m)[d(n)]  ] ，其中 b 和 n 在1-100之间。 \n\n " + \
                 "2. 使用 !dice 修改默认骰子的面数，不超过100，需以 ! 或 ！ 作为命令头。 \n\n " + \
                 "3. 使用 rhd 来进行暗骰，格式与明骰相同，结果将通过私聊发送。"
 
