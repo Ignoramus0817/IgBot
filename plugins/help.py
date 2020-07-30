a4363ebc65ebb819b3a35ac6254adf04fd1f2634
Ignoramus <Ignoramus0817@gmail.com>
2020-07-30 21:49:28 +0800

    modify help text

--- plugins/help.py
+++ plugins/help.py
@@ -9,7 +9,7 @@ async def helper(session: CommandSession):
                    "2. 除单独说明的命令外，其余命令均可用   /   !   ／   ！   .   作为命令头。"
 
     help_text = "【这里是正文】 \n\n " + \
-                "1. 使用 rd（或roll、random）  来进行投点，格式为 .rd  (a)d(b)  [+  (m)[d(n)]  ] ，其中 b 和 n 在1-100之间。 \n\n " + \
+                "1. 使用 rd（或roll、random）  来进行投点，格式为 .rd (a)d(b)[+(m)[d(n)]] ，其中 b 和 n 在1-100之间。 \n\n " + \
                 "2. 使用 !dice 修改默认骰子的面数，不超过100，需以 ! 或 ！ 作为命令头。 \n\n " + \
                 "3. 使用 rhd （或hd） 来进行暗骰，格式与明骰相同，结果将通过私聊发送。\n\n" + \
                 "4. 使用 help（或h） 来查看本说明。"
